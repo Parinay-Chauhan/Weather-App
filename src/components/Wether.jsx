be1ecd481941605011c46678e957d2d047862a29
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-20 20:01:48 +0530

    Refactor weather component: add weather icons and improve layout

--- src/components/Wether.jsx
+++ src/components/Wether.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Wether.css'
 import search_icon from "../assets/search.png"
+import clear_icon from "../assets/clear.png"
+import cloud_icon from "../assets/cloud.png"
+import drizzle_icon from "../assets/drizzle.png"
+import rain_icon from "../assets/rain.png"
+import snow_icon from "../assets/snow.png"
+import wind_icon from "../assets/wind.png"
+import humidity_icon from "../assets/humidity.png"
 
 const Wether = () => {
     return (
@@ -10,7 +17,9 @@ const Wether = () => {
                 <img src={search_icon} alt="" />
             </div>
 
-            <img src="" alt="" />
+            <img src={clear_icon} alt="" className='weather-icon' />
+            <p className='temperature'>16°c</p>
+            <p className='location'>Delhi</p>
         </div>
     )
 }
