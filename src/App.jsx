753ed1c0c93fb4a04d175bc0788b3fcdfd80f955
Parinay Chauhan <parinay07chauhan2006@gmail.com>
2026-08-20 19:34:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Wether from './components/Wether'
+
+const App = () => {
+  return (
+    <div>
+      <Wether />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Wether.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Wether.css'
+
+const Wether = () => {
+  return (
+    <div className='weather'>
+      <div className='search-bar'>
+         <input type="text" placeholder='Search'/>
+      </div>
+    </div>
+  )
+}
+
+export default Wether
